2f61e2bdd274ba81e9dbfafcfdbb673a788a39cb
Erdaulet0341 <erdaulet0341@gmail.com>
2023-03-02 10:52:52 +0600

    main

--- Lab6/HTTP_modelu/src/app/app-routing.module.ts
+++ Lab6/HTTP_modelu/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'albums', component: AlbumsComponent},
+  {path: 'about', component: AboutComponent},
+  {path: '', component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
